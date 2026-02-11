c8ad173630eb2aa35145b72d5674b5d454850bc7
PaigeN2026 <Paige.nunez2026@gbwl.org>
2026-02-11 14:37:43 +0000

    2/11

--- app.py
+++ app.py
@@ -9,8 +9,17 @@ app = Flask(__name__)
 @app.route("/")
 def index(): 
     # Pass variables from Python to HTML
-    name_data = None
-    return render_template("index.html", name=name_data)
+    name_data = 'Paige'
+    year_data = 2026
+    # Can also use lists & dictionaries 
+    favorites_list = ['Jo', 'Wo', 'No', 'Ko']
+    ratings_dict = { 
+                    'Jo' : 3, 
+                    'Wo' : 5,
+                    'No' : 9,
+                    'Ko' : 1
+                    }
+    return render_template("index.html", name=name_data, year=year_data, favorites=favorites_list, ratings=ratings_dict)
 
 # TO RUN YOUR APP enter "flask run" into the TERMINAL
 # TO STOP click CTRL + C in the TERMINAL 
