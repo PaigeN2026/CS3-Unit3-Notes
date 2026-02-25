29abc98e4c85ffb3f13c49ed7cbd8bc80000e3c1
PaigeN2026 <Paige.nunez2026@gbwl.org>
2026-02-25 14:40:17 +0000

    3.2 notes

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask import render_template
+from flask import render_template, request
 
 # Create an instance of Flask
 app = Flask(__name__)
@@ -23,6 +23,22 @@ def index():
                     "10 Things I Hate About you" : ""
                     }
     return render_template("index.html", name=name_data, year=year_data, favorites=favorites_list, ratings=ratings_dict)
+
+# Function to handle form submission
+# the app.route decorate maps to the submit button 
+@app.route("/submit", methods=['POST'])
+def submit():
+    # Creat python variable to hold form data 
+    form_data = {
+        'name': request.form.get('name'),
+        'age': request.form.get('age'),
+        'hobby': request.form.get('favorite_hobby'),
+        'color': request.form.get('favorite_color'),
+        'number': request.form.get('lucky_number')
+    }
+    # Pass data into results page
+    return render_template('results.html', form_data=form_data)
+
 
 # TO RUN YOUR APP enter "flask run" into the TERMINAL
 # TO STOP click CTRL + C in the TERMINAL 
